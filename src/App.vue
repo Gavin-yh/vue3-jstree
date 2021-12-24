3a6506e654276122c268e6fd47c4ae5d294afdfc
Gavin <1490259042@qq.com>
2021-12-24 11:10:23 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <tree-item msg="Vue3 js tree" />
+</template>
+
+<script setup lang="ts">
+import TreeItem from './components/tree-item.vue'
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tree-item.vue
@@ -0,0 +1,19 @@
+<template>
+  组件{{ count }}
+
+  <p>{{ props.msg }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  msg: string
+}>()
+
+const count = ref(0)
+</script>
+
+<style scoped lang="scss">
+
+</style>
